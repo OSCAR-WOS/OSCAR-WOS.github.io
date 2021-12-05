2b1af76fff8f7a2b917f0021caec8ad3f7300685
Oscar <29248751+OSCAR-WOS@users.noreply.github.com>
2021-12-05 00:06:46 +0000

    a

--- screenshot.py
+++ screenshot.py
@@ -1,5 +1,8 @@
 import json
+import asyncio
+import time
 from pyppeteer import launch
+from trim import trim_image, make_gif
 
 with open('screenshot.json') as f:
     data = json.load(f)
@@ -9,5 +12,15 @@ async def screenshot(url=None):
     browser = await launch({'headless': 'false'})
     page = await browser.newPage()
     await page.goto(url)
-    await page.screenshot(data)
+
+    for i in range(10):
+        data['path'] = f'screenshots/{i}.png'
+        await page.screenshot(data)
+        trim_image(f'screenshots/{i}.png', f'screenshots_trimmed/{i}.png')
+        time.sleep(1)
+
     await browser.close()
+    make_gif()
+
+asyncio.get_event_loop().run_until_complete(
+    screenshot('https://oscar-wos.github.io'))
---
+++ trim.py
@@ -0,0 +1,28 @@
+import numpy as np
+import glob
+from PIL import Image
+
+
+def trim_image(path="screenshots/0.png", out="screenshots/0_trim.png"):
+    image = Image.open(path)
+    image.load()
+
+    image_data = np.asarray(image)
+    image_data_bw = image_data.max(axis=2)
+    non_empty_columns = np.where(image_data_bw.max(axis=0) > 0)[0]
+    non_empty_rows = np.where(image_data_bw.max(axis=1) > 0)[0]
+    c = (min(non_empty_rows), max(non_empty_rows), min(
+        non_empty_columns), max(non_empty_columns))
+
+    image_data_new = image_data[c[0]:c[1]+1, c[2]:c[3]+1, :]
+
+    new_image = Image.fromarray(image_data_new)
+    new_image.save(out)
+
+
+def make_gif(frame_folder="screenshots_trimmed"):
+    frames = [Image.open(image)
+              for image in glob.glob(f'{frame_folder}/*.png')]
+    frame_one = frames[0]
+    frame_one.save('fetch.gif', format="GIF", append_images=frames,
+                   save_all=True, duration=100, loop=0)
